40544f0436b7085d048ff6d55536d45161d8501a
shadowbq <shadowbq@gmail.com>
2017-08-01 14:57:32 -0400

    more spike efforts

--- bin/arctic-phase.py
+++ bin/arctic-phase.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import yaml
 import sys
+import time
 import re
 import json
 import logging
 import os
+import tempfile
+
+from watchdog.observers import Observer
+import watchdog.events
 
 from dxlclient.client import DxlClient
 from dxlclient.client_config import DxlClientConfig
@@ -15,7 +20,7 @@ from dxltieclient.constants import HashType, ReputationProp, FileProvider, FileE
 VERBOSE = 0
 
 LOCAL_SAMPLES="samples/"
-LOCAL_RESULTS="var/log/suricata/"
+LOCAL_RESULTS="/opt/opendxl-arctic-phase/var/log/suricata/"
 
 # Import common logging and configuration
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../src/")
@@ -34,35 +39,81 @@ def verbose(x, y=0):
     else:
         0 #No Op
 
+class TIEHandler(watchdog.events.PatternMatchingEventHandler):
 
-# Create the client
-with DxlClient(config) as client:
+    def on_created(self, event):
+        print "Looking at ", event.src_path
+        #try:
+        tieLookup(event.src_path)
+        #except:
+        #  print "invalid file"
 
-  # Connect to the fabric
-  client.connect()
+class ScanFolder:
 
-  # Create the McAfee Threat Intelligence Exchange (TIE) client
-  tie_client = TieClient(client)
+  def __init__(self, options={}):
+    self.options = options
+    #self.path = options.directory
+    self.path = LOCAL_RESULTS + "files/"
+    self.event_handler = TIEHandler(patterns=["*.meta"], ignore_patterns=[], ignore_directories=True)
+    self.observer = Observer()
+    print self.path
+    self.observer.schedule(self.event_handler, self.path, recursive=True)
+    self.observer.start()
+
+  def stop(self):
+    self.observer.stop()
+    self.observer.join()
+    #os.rmdir(self.temp_dir)
+
+  def get_filepaths(self, directory):
+  
+    file_paths = []
+    for root, directories, files in os.walk(directory):
+      files = [ f for f in files if not f[0] == '.']
+      directories[:] = [d for d in directories if not d[0] == '.']
+      for filename in files:
+        filepath = os.path.join(root, filename)
+        file_paths.append(filepath)
+
+    return file_paths 
 
 
-  with open(LOCAL_RESULTS + "files/file.44.meta", 'r') as stream:
+def printTIE(reputations_dict):
+  # Display the Global Threat Intelligence (GTI) trust level for the file
+  if FileProvider.GTI in reputations_dict:
+      gti_rep = reputations_dict[FileProvider.GTI]
+      print "Global Threat Intelligence (GTI) trust level: " + \
+            str(gti_rep[ReputationProp.TRUST_LEVEL])
+
+  # Display the Enterprise reputation information
+  if FileProvider.ENTERPRISE in reputations_dict:
+      ent_rep = reputations_dict[FileProvider.ENTERPRISE]
+
+      print "Threat Intelligence Exchange (Local) trust level: " + \
+            str(ent_rep[ReputationProp.TRUST_LEVEL])
+
+      # Retrieve the enterprise reputation attributes
+      ent_rep_attribs = ent_rep[ReputationProp.ATTRIBUTES]
+
+      # Display prevalence (if it exists)
+      if FileEnterpriseAttrib.PREVALENCE in ent_rep_attribs:
+          print "Enterprise prevalence: " + \
+                ent_rep_attribs[FileEnterpriseAttrib.PREVALENCE]
+
+      # Display first contact date (if it exists)
+      if FileEnterpriseAttrib.FIRST_CONTACT in ent_rep_attribs:
+          print "First contact: " + \
+                FileEnterpriseAttrib.to_localtime_string(
+                    ent_rep_attribs[FileEnterpriseAttrib.FIRST_CONTACT])
+
+
+def tieLookup(filename):
+  with open(filename, 'r') as stream:
     try:
         dataMap = yaml.load(stream)
     except yaml.YAMLError as exc:
       print(exc)
-
-  #print ""
-  #print "=-----------="
-  #print "dataMap is a ", type(dataMap), dataMap
-  verbose("=-----------=")
-  verbose("File State: {0}".format(dataMap['STATE']))
-  verbose("MD5: {0}".format(dataMap['MD5']))
-  verbose("SHA1: {0}".format(dataMap['SHA1']))
-  verbose("MAGIC: {0}".format(dataMap['MAGIC']))
-  verbose("=-----------=")
-
   if dataMap['STATE'] == 'CLOSED':
-
     #print "the file is closed"
     p = re.compile('PE32')
     match = p.match(dataMap['MAGIC'])
@@ -79,34 +130,41 @@ with DxlClient(config) as client:
         output = json.dumps(reputations_dict, sort_keys=True, indent=4, separators=(',', ': ')) + "\n"
         verbose(output, 2)
 
-        # Display the Global Threat Intelligence (GTI) trust level for the file
-        if FileProvider.GTI in reputations_dict:
-            gti_rep = reputations_dict[FileProvider.GTI]
-            print "Global Threat Intelligence (GTI) trust level: " + \
-                  str(gti_rep[ReputationProp.TRUST_LEVEL])
+        try:
+            fo = open(filename + ".verdict", "w")
+            fo.write(output)
+            fo.close()
+        except:
+            print "could not write to directory"
+            
+ 
+        printTIE(reputations_dict)
 
-        # Display the Enterprise reputation information
-        if FileProvider.ENTERPRISE in reputations_dict:
-            ent_rep = reputations_dict[FileProvider.ENTERPRISE]
-
-            print "Threat Intelligence Exchange (Local) trust level: " + \
-                  str(ent_rep[ReputationProp.TRUST_LEVEL])
-
-            # Retrieve the enterprise reputation attributes
-            ent_rep_attribs = ent_rep[ReputationProp.ATTRIBUTES]
-
-            # Display prevalence (if it exists)
-            if FileEnterpriseAttrib.PREVALENCE in ent_rep_attribs:
-                print "Enterprise prevalence: " + \
-                      ent_rep_attribs[FileEnterpriseAttrib.PREVALENCE]
-
-            # Display first contact date (if it exists)
-            if FileEnterpriseAttrib.FIRST_CONTACT in ent_rep_attribs:
-                print "First contact: " + \
-                      FileEnterpriseAttrib.to_localtime_string(
-                          ent_rep_attribs[FileEnterpriseAttrib.FIRST_CONTACT])
 
     else:
         print 'No Match'
 
-sys.exit(0)
+
+
+
+
+# Create the client
+with DxlClient(config) as client:
+
+  # Connect to the fabric
+  client.connect()
+
+  # Create the McAfee Threat Intelligence Exchange (TIE) client
+  tie_client = TieClient(client)
+
+  # Watch directory loop
+
+  job = ScanFolder()
+
+  try: 
+      while True:
+            time.sleep(1)
+  except KeyboardInterrupt:
+      job.stop()
+      sys.exit(0)
+
