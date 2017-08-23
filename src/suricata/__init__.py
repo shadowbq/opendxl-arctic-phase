cb41663f0f40f87a85931b7f8a3deb35dbd070bd
shadowbq <shadowbq@gmail.com>
2017-08-23 11:10:39 -0400

    eve test

--- src/suricata/__init__.py
+++ src/suricata/__init__.py
@@ -1,4 +1,6 @@
 # Suricata methods
+import os
+import json
 
 def addtosuricatablacklist(md5):
   try:
@@ -8,3 +10,28 @@ def addtosuricatablacklist(md5):
       blacklist_fh.close()
   except:
       print "suricata blacklist file is not available"
+
+
+class SuricataEve(object):
+
+    def __init__(self, options):
+        eve_dict = \
+          {
+              "timestamp": "2009-11-24T21:27:09.534255",
+              "event_type": "alert",
+              "src_ip": "192.168.2.7",
+              "src_port": 1041,
+              "dest_ip": "x.x.250.50",
+              "dest_port": 80,
+              "proto": "TCP",
+              "alert": {
+                  "action": "allowed",
+                  "gid": 1,
+                  "signature_id" :2001999,
+                  "rev": 9,
+                  "signature": "ET MALWARE BTGrab.com Spyware Downloading Ads",
+                  "category": "A Network Trojan was detected",
+                  "severity": 1
+              }
+          }
+        print json.dumps(eve_dict, sort_keys=True, indent=4, separators=(',', ': '))
